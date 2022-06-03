d5c1412d78445d3a433aa7444cf46f60d3c6fc9b
Joel C <joel7921111@yahoo.com>
2022-06-03 15:17:16 -0600

    thoughts, reactions routes tested, populating works

--- routes/thought-routes.js
+++ routes/thought-routes.js
@@ -1,9 +1,16 @@
-const { Thought } = require("../models");
+const { Thought, User } = require("../models");
 const router = require("express").Router();
 
 // create new thought
-router.post("/create", ({ body }, res) => {
+router.post("/create/:userId", ({ params, body }, res) => {
   Thought.create(body)
+    .then(({ _id }) => {
+      return User.findOneAndUpdate(
+        { _id: params.userId },
+        { $push: { thoughts: _id } },
+        { new: true }
+      );
+    })
     .then((dbThoughtData) => res.json(dbThoughtData))
     .catch((err) => res.status(400).json(err));
 });
@@ -11,6 +18,12 @@ router.post("/create", ({ body }, res) => {
 // get all thoughts
 router.get("/all", (req, res) => {
   Thought.find({})
+    .populate({
+      path: "reactions",
+      select: "-__v",
+    })
+    .select("-__v")
+    .sort({ _id: -1 })
     .then((dbThoughtData) => {
       res.json(dbThoughtData);
     })
@@ -22,6 +35,11 @@ router.get("/all", (req, res) => {
 // get one thought by id
 router.get("/:id", ({ params }, res) => {
   Thought.findOne({ _id: params.id })
+    .populate({
+      path: "reactions",
+      select: "-__v",
+    })
+    .select("-__v")
     .then((dbThoughtData) => {
       if (!dbThoughtData) {
         res.status(404).json({ message: "No thought found with this id!" });
@@ -61,4 +79,26 @@ router.delete("/delete/:id", ({ params }, res) => {
     .catch((err) => res.status(400).json(err));
 });
 
+// create new reaction
+router.post("/:thoughtId/reactions", ({ body, params }, res) => {
+  console.log(body, "string");
+  Thought.findOneAndUpdate(
+    { _id: params.thoughtId },
+    { $push: { reactions: body } },
+    { new: true }
+  )
+    .then((dbFriendData) => res.json(dbFriendData))
+    .catch((err) => res.status(400).json(err));
+});
+
+// create new reaction
+router.delete("/:thoughtId/reactions/:reactionId", ({ params }, res) => {
+  Thought.findOneAndUpdate(
+    { _id: params.thoughtId },
+    { $pull: { reactions: params.reactionId } },
+    { new: true }
+  )
+    .then((dbFriendData) => res.json(dbFriendData))
+    .catch((err) => res.status(400).json(err));
+});
 module.exports = router;
